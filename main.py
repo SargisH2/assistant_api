217b86e4850362bc95e9936a93e14366060245dd
Sargis <sargishunanyan02@gmail.com>
2023-11-22 11:38:32 +0400

    error fixing

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from serpapi import GoogleSearch
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+ASSISTANT_ID = 'asst_8VWwHooSU70gUK7vHkHvnSRB'
 
 OPENAI_API_KEY = "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"
 SERPAPI_API_KEY = 'd277c1be0ca1168875f9e3d817e0b17ff3e3b8b21cbce42dbe10938116c73cb5'
 
-ASSISTANT_ID = 'asst_8VWwHooSU70gUK7vHkHvnSRB'
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+os.environ['SERPAPI_API_KEY'] = SERPAPI_API_KEY
 
 client = OpenAI()
 assistant = client.beta.assistants.retrieve(ASSISTANT_ID)
